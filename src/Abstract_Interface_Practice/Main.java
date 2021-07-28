2f170af85f319904de9a88e5dcee3b624bea0c2a
Patrick Agenonga <pjagenonga@gmail.com>
2021-07-27 20:14:16 -0700

    Java Practice - Linked List

--- src/Abstract_Interface_Practice/Main.java
+++ src/Abstract_Interface_Practice/Main.java
@@ -57,7 +57,7 @@ public class Main {
     //
 
     SearchTree tree = new SearchTree(null);
-        tree.traverse(tree.getRoot());
+    tree.traverse(tree.getRoot());
     // Create a string data array to avoid typing loads of addItem instructions:
     String stringData = "5 7 3 9 8 2 1 0 4 6";
 //        String stringData = "Darwin Brisbane Perth Melbourne Canberra Adelaide Sydney Canberra";
